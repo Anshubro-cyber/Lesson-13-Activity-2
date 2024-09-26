40473557a05cf84254dde46de35fb2fbd5949ff1
Anshubro-cyber <akshatagrawal0831@gmail.com>
2024-09-26 18:05:35 +0530

    Lesson-13 Activity-2

---
+++ main.py
@@ -0,0 +1,26 @@
+file_read = open('Abcd.txt','r')
+print("File in Read Mode -")
+print(file_read.read())
+file_read.close()
+
+file_write = open('Abcd.txt','w')
+file_write.write("File in write mode .....")
+file_write.write("Hi! I am Akshat. I am 13 years old")
+file_write.write("This is file program")
+file_write.close()
+
+file_read = open('Abcd.txt','r')
+print("File in Read Mode -")
+print(file_read.read())
+file_read.close()
+
+file_append = open('Abcd.txt','a')
+file_append.write("/n File in append mode ......")
+file_append.write("Hi! I am Akshat. I am 13 years old")
+file_append.write("I am learning file handling now!!")
+file_append.close()
+
+file_read = open('Abcd.txt','r')
+print("File in Read Mode -")
+print(file_read.read())
+file_read.close()
